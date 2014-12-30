87b332c3f7e622d1a2241394cd3e74ea95692909
Andranik Andy Tonoyan <xdranik@gmail.com>
2014-12-29 17:03:20 -0800

    Replaced default grunt task code with a call to string-baker

--- tasks/string-baker.js
+++ tasks/string-baker.js
@@ -8,44 +8,28 @@
 
 'use strict';
 
+var stringBaker = require('./lib/string-baker.js');
+
 module.exports = function(grunt) {
 
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
-  grunt.registerMultiTask('string_baker', 'The best Grunt plugin ever.',
+  grunt.registerMultiTask(
+    'string_baker',
+    'The best Grunt plugin ever.',
     function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
+      var files = {
+        src: this.data.src,
+        dest: this.data.dest,
+        dataFiles: this.data.dataFiles
+      };
+      var replacements = this.data.replacements;
+      var options = this.options({
+        defaultKeyString: 'KEY'
+      });
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
-  });
-
+      stringBaker.bake(files, replacements, options);
+    }
+  );
 };
