729e691aad61a09a5ade63156f678e5c86f0ed93
Andranik Andy Tonoyan <xdranik@gmail.com>
2015-01-03 02:01:22 -0800

    moved data-file helper into lib/

--- tasks/lib/data-file.js
+++ tasks/lib/data-file.js
@@ -2,19 +2,22 @@
 
 var path = require('path');
 var _ = require('lodash');
-var parser = require('../parser');
-var utils = require('../utils');
+var parser = require('./parser');
+var utils = require('./util');
+var DataFile = {};
 
-exports.getContext = function(files) {
-  var context = exports.generateContext(files);
+DataFile.getContext = function(files) {
+  var context = DataFile.generateContext(files);
   var flattenedContext = utils.flattenObject(context);
 
   return flattenedContext;
 };
 
-exports.generateContext = function(files) {
+DataFile.generateContext = function(files) {
   var contextList = _.map(files, parser.parse);
   var context = _.merge.apply(null, contextList);
 
   return context;
 };
+
+module.exports = DataFile;
